ac2a2b9de300a777ef43f9849bc4066de0b8e2f6
komalkubsad <komalkubsad@192.168.0.105>
2017-10-09 15:36:07 -0400

    DiningMgmtSystem_1

---
+++ DiningMgmt/src/main/java/edu/uncg/dining/mgmt/models/Payroll.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uncg.dining.mgmt.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author komalkubsad
+ */
+@Entity 
+@Table (name = "Payroll")
+public class Payroll {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long employeeId;
+    private String employeeName;
+    private int dateOfJoining;
+    private int workingHours;
+    private int totalPay;
+    private int status;
+
+    public long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public int getDateOfJoining() {
+        return dateOfJoining;
+    }
+
+    public void setDateOfJoining(int dateOfJoining) {
+        this.dateOfJoining = dateOfJoining;
+    }
+
+    public int getWorkingHours() {
+        return workingHours;
+    }
+
+    public void setWorkingHours(int workingHours) {
+        this.workingHours = workingHours;
+    }
+
+    public int getTotalPay() {
+        return totalPay;
+    }
+
+    public void setTotalPay(int totalPay) {
+        this.totalPay = totalPay;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+    
+    
+    
+}
---
+++ DiningMgmt/src/main/java/edu/uncg/dining/mgmt/repositories/PayrollRepository.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uncg.dining.mgmt.repositories;
+
+import edu.uncg.dining.mgmt.models.Payroll;
+
+/**
+ *
+ * @author komalkubsad
+ */
+public interface PayrollRepository extends JpaRepository<Payroll, int> {
+    
+}
